051a930ebd32ff68056ac542aef934f6ff09821d
Mubashir Tanwar <tanwar0210@gmail.com>
2024-10-26 03:40:42 +0530

    day25 finished CRUD API

--- 25mongodb/controller/controller.go
+++ 25mongodb/controller/controller.go
@@ -3,9 +3,12 @@ package controller
 import (
 	"25mongodb/model"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -24,7 +27,7 @@ func init() {
 
 	// connect to mongodb
 	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Connection Established")
@@ -38,7 +41,7 @@ func init() {
 // Mongo helpers -> File
 
 // insert 1 record
-func insertOneMovie(movie model.Netflix){
+func insertOneMovie(movie model.Netflix) {
 	success, error := collection.InsertOne(context.Background(), movie)
 	if error != nil {
 		log.Fatal(error)
@@ -46,7 +49,7 @@ func insertOneMovie(movie model.Netflix){
 	fmt.Println("Inserted One Movie", success.InsertedID)
 }
 
-func updateOneMovie(movieId string){
+func updateOneMovie(movieId string) {
 	id, _ := primitive.ObjectIDFromHex(movieId)
 
 	filter := bson.M{"_id": id}
@@ -57,5 +60,84 @@ func updateOneMovie(movieId string){
 		log.Fatal(err)
 	}
 	fmt.Println("updated:", result.ModifiedCount)
-	
+
+}
+
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+
+	filter := bson.M{"_id": id}
+
+	result, err := collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("updated:", result.DeletedCount)
+}
+
+func deleteAll() int64 {
+	filter := bson.D{}
+	result, err := collection.DeleteMany(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("updated:", result.DeletedCount)
+	return result.DeletedCount
+}
+
+func getAllMovies()[]primitive.M {
+	cur, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(cur)
+
+	var movies []primitive.M
+
+	for cur.Next(context.Background()) {
+		var movie bson.M
+		fmt.Println(movie)
+		if err := cur.Decode(&movie); err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+	defer cur.Close(context.Background())
+	return movies
+}
+
+func GetAllMovies(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(getAllMovies())
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+
+	var movie model.Netflix
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	insertOneMovie(movie)
+	json.NewEncoder(w).Encode("Movie added successfully")
+
+}
+
+func MarkAsWatched(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	updateOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteOneMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	deleteOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAllMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	deleteAll()
+	json.NewEncoder(w).Encode("Deleted")
 }
--- 25mongodb/main.go
+++ 25mongodb/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"25mongodb/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("We are connecting mongo db to go")
+
+	r := router.Router()
+	log.Fatal(http.ListenAndServe(":4000", r))
+
+	fmt.Println("Listening to port 4000")
 }
